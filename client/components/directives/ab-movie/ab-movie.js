5eb8d635e22b8d620cbd3dd81ba8e6e42f9a50a9
Adam Barnhard <admbarn@gmail.com>
2014-09-17 14:43:59 -0500

    added movies

---
+++ client/components/directives/ab-movie/ab-movie.js
@@ -0,0 +1,41 @@
+/* jshint camelcase:false */
+
+(function(){
+  'use strict';
+
+  angular.module('abMovieModule', [])
+  .factory('MovieApi', ['$http', function($http){
+    function getMovieId(movie){
+      movie = movie.replace(' ', '+');
+      return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q='+movie+'&page_limit=1&page=1&apikey=v2c29merm2sdj4hmrb3kspua&callback=JSON_CALLBACK');
+    }
+
+    function movieInfo(res){
+      var id = res.data.movies[0].id;
+      return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/'+id+'.json?apikey=v2c29merm2sdj4hmrb3kspua&callback=JSON_CALLBACK');
+    }
+
+    return {getMovieId:getMovieId, movieInfo:movieInfo};
+  }])
+  .directive('abMovie', [function(){
+    var o = {};
+
+    o.restrict    = 'A';
+    o.templateUrl = '/components/directives/ab-movie/ab-movie.html';
+    o.scope       = {title:'@'};
+    o.link        = function(scope, element, attrs){
+                    };
+    o.controller  = ['$scope', 'MovieApi', function($scope, MovieApi){
+                      MovieApi.getMovieId($scope.title).then(MovieApi.movieInfo).then(function(res){
+                        console.log(res.data);
+                        $scope.movieTitle = res.data.title;
+                        $scope.posterUrl = res.data.posters.detailed.replace('_tmb', '_det');
+                        $scope.release = res.data.release_dates.theater;
+                        $scope.rating = res.data.mpaa_rating;
+                        $scope.length = res.data.runtime;
+                        $scope.actors = res.data.abridged_cast.map(function(a){return a.name});
+                      });
+                    }];
+    return o;
+  }]);
+})();
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  angular.module('dirdemo', ['ngRoute', 'LocalForageModule', 'abGreetingModule', 'abClockModule', 'abStockModule', 'abWeatherModule'])
+  angular.module('dirdemo', ['ngRoute', 'LocalForageModule', 'abGreetingModule', 'abClockModule', 'abStockModule', 'abWeatherModule', 'abMovieModule'])
   .config(['$routeProvider', '$httpProvider', '$localForageProvider', function($routeProvider, $httpProvider, $localForageProvider){
     $routeProvider
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
